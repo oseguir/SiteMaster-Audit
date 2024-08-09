6cfe7cbe20bdfbe8e9ded4565b344080632cb6e8
Apasionado <58598786+oseguir@users.noreply.github.com>
2024-08-09 13:53:26 +0200

    install.py

---
+++ install.py
@@ -0,0 +1,9 @@
+# Librerias necesarias para el uso de SiteMaster (instalación auto)
+
+pip install --upgrade pip
+pip install requests beautifulsoup4 cssutils colour tenacity
+pip install requests
+pip install beautifulsoup4
+pip install cssutils
+pip install colour
+pip install tenacity
